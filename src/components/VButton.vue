88e91b453bea5d29886a245cdec8396b59df4c16
Erik <erikbitner+git@gmail.com>
2021-02-25 15:21:27 +0700

    feat: add global button component

---
+++ src/components/VButton.vue
@@ -0,0 +1,47 @@
+<template lang="pug">
+button.button(
+    :type="type",
+    :disabled="disabled",
+)
+    slot
+</template>
+
+<script>
+export default {
+    props: {
+        type: String,
+        disabled: Boolean
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.button {
+    cursor: pointer;
+    width: 100%;
+    height: 52px;
+    border: 0;
+    outline: 0;
+
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 16px;
+    letter-spacing: 1px;
+    text-transform: uppercase;
+
+    color: $color_white;
+
+    background: $color_accent-green;
+    border-radius: 4px;
+    transition: background 0.2s;
+
+    &:hover, &:active {
+        background: darken(saturate($color_accent-green, 14.33), 10.00);
+    }
+
+    &:disabled {
+        cursor: initial;
+        background: $color_icon_gray;
+    }
+}
+</style>
